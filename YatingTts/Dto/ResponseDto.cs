40a6a5750813cdbef0e1583e5215c90fdea4e8b4
pinke <pinke.yu7@gmail.com>
2023-08-24 17:09:05 +0800

    Feature: add yating tts client

---
+++ YatingTts/Dto/ResponseDto.cs
@@ -0,0 +1,13 @@
+﻿using System.Text.Json.Serialization;
+
+namespace YatingTts.Dto
+{
+    public class ResponseDto
+    {
+        [JsonPropertyName("audioContent")]
+        public string AudioContent { get; set; }
+        [JsonPropertyName("audioConfig")]
+        public AudioConfigDto AudioConfig { get; set; }
+    }
+}
+
